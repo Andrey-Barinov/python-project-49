76ae467566282379342de9a6bede1e9ad85af06b
Andrey-Barinov <barbpro89@gmail.com>
2023-10-25 19:20:04 +0300

    Third step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import sys
+
+sys.path.append('/home/barbpro/project1/python-project-49/brain_games/')
+
+from cli import welcome_user
+
 def main():
     print('Welcome to the Brain Games')
-
+    welcome_user()
 
 if __name__ == '__main__':
     main()
