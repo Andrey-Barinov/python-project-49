698d082576de656feab7e7616724c234a5b67f7b
Andrey-Barinov <barbpro89@gmail.com>
2023-11-02 18:35:35 +0300

    try to fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
-import sys
 
-sys.path.append("/home/barbpro/project1/python-project-49/brain_games/")
-
-from cli import welcome_user
+from brain_games.cli import welcome_user
 
 
 def main():
